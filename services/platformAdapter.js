c95793282a8212aaa2fcfccf624de26ac48e2237
EthanVT97 <gyi798107@gmail.com>
2025-05-28 03:37:17 +0630

    Update platformAdapter.js

--- services/platformAdapter.js
+++ services/platformAdapter.js
@@ -7,16 +7,25 @@ async function updateViberBotInfo(realToken, updateData) {
   if (updateData.name) payload.name = updateData.name;
   if (updateData.avatar) payload.avatar = updateData.avatar;
 
+  // Make sure there is something to update
+  if (Object.keys(payload).length === 0) {
+    throw new Error('No valid fields to update');
+  }
+
   const headers = {
     'X-Viber-Auth-Token': realToken,
     'Content-Type': 'application/json',
   };
 
   const response = await axios.post(url, payload, { headers });
+
   if (response.data.status !== 0) {
     throw new Error(`Viber API error: ${response.data.status_message || 'Unknown error'}`);
   }
+
   return response.data;
 }
 
-module.exports = { updateViberBotInfo };
+module.exports = {
+  updateViberBotInfo,
+};
